7f31c74c8c5b4bc6721307ca0805edcd8ed06968
Jacenljz <jacenliu0703@gmail.com>
2026-05-16 16:05:58 -0700

    111

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const btn = document.getElementById('homeButton');
 btn.addEventListener('click', function() {
-    window.location.href = 'about.html';
+    window.location.href = 'tool.html';
 });
+const prof = document.getElementsByClassName('proffile');
+prof.addEventListener('click',function()){
+    window.location.href = 'about.html';
+}
