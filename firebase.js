e126cbaba1cde265a6a0c9e62b39a4564bf3ab2a
Jacenljz <jacenliu0703@gmail.com>
2026-05-17 09:43:48 -0700

    1

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHTafFTnh8O3b2AH1K54oOxuzLMzutL4E",
+    authDomain: "hakathon-2026.firebaseapp.com",
+    databaseURL: "https://hakathon-2026-default-rtdb.firebaseio.com",
+    projectId: "hakathon-2026",
+    storageBucket: "hakathon-2026.firebasestorage.app",
+    messagingSenderId: "909528413417",
+    appId: "1:909528413417:web:ab0d52870763228f12ec78",
+    measurementId: "G-GRCB9FXS3C"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+import { auth } from './firebase.js';
+import { onAuthStateChanged, signOut }
+    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 const btn = document.getElementById('homeButton');
-btn.addEventListener('click', function() {
-    window.location.href = 'tool.html';
+
+let currentUser = null;
+
+onAuthStateChanged(auth, function(user) {
+    currentUser = user;
+    const nameSpan = document.querySelector('.profile span');
+    if (nameSpan) {
+        nameSpan.textContent = user ? (user.email.split('@')[0]) : 'Sign In';
+    }
 });
-const prof = document.getElementsByClassName('profile');
-prof.addEventListener('click',function() {
-    window.location.href = 'about.html';
+
+btn.addEventListener('click', function() {
+    if (currentUser) {
+        window.location.href = 'tool.html';
+    } else {
+        window.location.href = 'login.html?redirect=index.html';
+    }
 });
