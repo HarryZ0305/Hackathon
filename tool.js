39848b666911cd85d577f507e4720594f504d9dc
Harry Zhou <harryzhou935@gmail.com>
2026-05-17 09:28:07 -0700

    Occasion screen

---
+++ tool.js
@@ -0,0 +1,41 @@
+const opportunities = [
+    { 
+        title: "Event 1 Name", 
+        data: "Month Date • City, State or Online", 
+        description: "Description 1" 
+    },
+    { 
+        title: "Event 2 Name", 
+        data: "Month Date • City, State or Online", 
+        description: "Description 2" 
+    },
+    { 
+        title: "Event 3 Name", 
+        data: "Month Date • City, State or Online", 
+        description: "Description 3" 
+    }
+];
+
+let index = 0;
+
+const titleElement = document.getElementById('title');
+const dataElement = document.getElementById('data');
+const descriptionElement = document.getElementById('description');
+
+const nextButton = document.getElementById('nextBtn');
+
+nextButton.addEventListener('click', function() {
+    
+    index++;
+    if (index >= opportunities.length) {
+        index = 0;
+    }
+    
+    titleElement.textContent = opportunities[index].title;
+    dataElement.textContent = opportunities[index].data;
+    descriptionElement.textContent = opportunities[index].description;
+});
+
+titleElement.textContent = opportunities[index].title;
+dataElement.textContent = opportunities[index].data;
+descriptionElement.textContent = opportunities[index].description;
